b9b0118838c3700b1c0a82ad9995a491693b974d
Susan Davies <sdav9375@gmail.com>
2016-11-12 19:59:55 -0500

    stateless component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
+
+const Header = () => {
+
     return(
       <div>
         <h2>Loneliest Chatroom</h2>
         <h2>No friends? No problem!</h2>
       </div>
     )
-  }
+
 }
 
 export default Header;
